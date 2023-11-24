a2af87a110115d8799afcfcc8d32ef39b7ab353c
bilel zaaraoui <bilelzara@gmail.com>
2023-11-24 10:37:28 +0100

    2023-11-24-10h37-Bilel-ModifRouter
    correction de la route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WeatherPage from './pages/WeatherPage';
 import './App.css';
 
 function App() {
   return (
  <Router>
- <div>
-    <Switch>
-      <Route exact path="/">
-        <WeatherPage />
-      </Route>
-    </Switch>
- </div>
+    <Routes>
+      <Route path="/" element={<WeatherPage />} />
+    </Routes>
  </Router>
   ); 
 }
